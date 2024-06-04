3e8d13b4468db4d6c933911959f7710b11421ebb
davidrahabi <davidrahabi@gmail.com>
2024-06-04 15:46:06 -0400

    Chess board completed

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from const import *
+from game import Game
 
 class Main:
 
@@ -8,9 +9,12 @@ class Main:
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) #creates screen
         pygame.display.set_caption('Chess')
+        self.game = Game()
 
     def mainloop(self):
         while True:
+            self.game.show_bg(self.screen)  #this loop will keep running and continually update teh screen
+
             for event in pygame.event.get(): #looks through all possible game events (user actions)
                 if event.type ==pygame.QUIT: #if event is a quit by the user, end game
                     pygame.quit()
